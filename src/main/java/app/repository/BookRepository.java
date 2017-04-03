806f6e1e3f8cf1fe4d19f754673f20dd511b1580
villepaa <ville.paavola@gmail.com>
2017-04-03 08:03:30 +0300

    modified BookRepository, needs fix before use!

--- src/main/java/app/repository/BookRepository.java
+++ src/main/java/app/repository/BookRepository.java
@@ -1,8 +1,11 @@
-//
-//package app.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
+
+package app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 //public interface BookRepository extends JpaRepository<Book,Long> {
-//    
+//   
 //}
+public interface BookRepository extends JpaRepository<String,Long> {
+   
+}
