00b7c72a68a739ade274dd9e871cc0e5ea17dd29
mustikka2015 <iisa.katka@helsinki.fi>
2017-04-04 12:35:33 +0300

    Javadoc

--- src/main/java/app/domain/Book.java
+++ src/main/java/app/domain/Book.java
@@ -8,53 +8,57 @@ import javax.persistence.Id;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 import javax.persistence.DiscriminatorValue;
 
+/**
+ * This class is to create different book objects.
+ */
 @Entity
 @DiscriminatorValue(value = "Book")
-public class Book extends Reference  {
+public class Book extends Reference {
 
     @Id
     private Long id;
 
-
-//    private List<String> authors;  //Mikäli kirjoittajalista on ArrayList-muodossa
-    private String author;    //Mikäli kirjoittajalista on String-muodossa
+    /**
+     * There are two possibilities of storing authors: String and
+     * ArrayList<String>. It depends on the implementation, which of those will
+     * remain.
+     */
+//    private List<String> authors;  
+    private String author;
     private String title;
     private int year;
     private String publisher;
     private String address;
 
-    //Tässä kontruktorissa on kaikki mahdolliset kentät. Osoite-kenttä ei ollut pakollinen,
-    //mutta se löytyi yhdestä viitteestä.
-   
-
-//    //Tämä konstruktori on sitä varten, että useimmiten malliviitteissä ei ollut osoitetta.
-//    //Tässä asetetaan vain pakolliset kentät.
-//    public Book(String author, ArrayList<String> authors, String title, int year, String publisher) {
-//        //Joko:
-////        this.authors = authors;
-//        //Tai:
-//        this.author = author;
-//        this.title = title;
-//        this.year = year;
-//        this.publisher = publisher;
-//        this.address = "";
-//    }
-
+    /**
+     * This constructor contains all the possible knowledge fields of a book.
+     *
+     * @param address is not compulsory.
+     */
+    public Book(String author, ArrayList<String> authors, String title, int year, String publisher) {
+        
+//      this.authors = authors;
+        this.author = author;
+        this.title = title;
+        this.year = year;
+        this.publisher = publisher;
+        this.address = "";
+    }
+    
     @Override
     public String getTitle() {
         return this.title;
     }
 
-    //Mikäli halutaan palauttaa koko kirjoittajalista:
-  
-
-    //Tällä palautetaan n:s kirjoittaja.
+    /**
+     * The method returns the author at the position n on the list.
+     */
 //    public String getAuthor(int n) {
 //        return this.authors.get(n);
 //    }
-
-    //Mikäli käytetään String-muotoista versiota, jossa kirjoittajat
-    //ovat jo valmiiksi toString()-muodossa.
+    /**
+     * The method returns the authors, if they are saved as a String.
+     */
     @Override
     public String getAuthor() {
         return this.author;
@@ -79,12 +83,16 @@ public class Book extends Reference  {
         this.title = title;
     }
 
-    //Mikäli käytetään tekijöistä ArrayList-muotoa, tällä voi lisätä listaan kirjan n:nnen kirjoittajan.
+    /**
+     * The method inserts an author at the place n on the list. The possible
+     * previous author at the place n is shifted to the rigth. 
+     */
 //    public void setAuthor(String author, int n) {
 //        this.authors.add(n, title);
 //    }
-
-    //Tällä voi asettaa kaikki kirjoittajat, mikäli ne on String-muodossa.
+    /**
+     * The method sets the authors, if the authors are saved as a String..
+     */
     @Override
     public void setAuthor(String author) {
         this.author = author;
@@ -104,13 +112,12 @@ public class Book extends Reference  {
         this.address = address;
     }
 
-    //Tällä on tarkoitus tulostaa koko viite String-muodossa.
-    //Tässä oletan, että tekijät ovat listana valmiiksi String-muodossa.
-    // Mikäli tekijät talletetaan ArrayList-muodossa, voi tehdä oman metodin,
-    //jolla tekijät muutetaan String-muotoon.
-    //Katsoin viitteen mallia tehtävänannosta.
-    //Jossain viitteistä oli osoite, muttei kaikissa, sen tähden if-lause.
-    //Elina on ekspertti viitteen oikean muodon suhteen...
+    /**
+     * This method returns all the information of the book referenced.
+     * The if-sentence allows the address field to be empty.
+     *
+     * @return The reference information of a book as a String.
+     */
     @Override
     public String toString() {
         String tulostus = this.author + ". " + this.title + ". " + this.publisher + ", " + this.year + ".";
@@ -120,10 +127,15 @@ public class Book extends Reference  {
         return tulostus;
     }
 
-    //Tällä on tarkoitus tulostaa koko viite BibTex-muodossa.
+    /**
+     * This method returns all the information of the book referenced as a
+     * BibTex String.
+     *
+     * @return The BibTex-reference information of a book as a String.
+     */
     @Override
     public String toBibTex() {
-        return "";
+        return "Not supported yet";
     }
 
     public Long getId() {
