a42c9f8f6e3f6eb2916711f96f79c437b6d46cd4
Tatu Helander <tatu.helander@helsinki.fi>
2017-04-05 12:57:25 +0300

    Cucumber

---
+++ src/test/java/app/Stepdefs.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ *
+ * @author tatuhelander
+ */
+public class Stepdefs {
+    
+    
+    
+    @Given("^form book is selected$")
+    public void form_book_is_selected() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^title \"([^\"]*)\" authro \"([^\"]*)\" publisher \"([^\"]*)\" year \"([^\"]*)\" are insterted$")
+    public void title_authro_publisher_year_are_insterted(String arg1, String arg2, String arg3, String arg4) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^submit is pressed$")
+    public void submit_is_pressed() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^system will respond with \"([^\"]*)\"$")
+    public void system_will_respond_with(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
