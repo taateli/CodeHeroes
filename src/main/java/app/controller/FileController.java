15a874aa02479ac5b354b92577b2af2efee06d23
Kaisa Sarén <kaisa.saren@gmail.com>
2017-04-11 15:51:42 +0300

    .bib nappi tehty

--- src/main/java/app/controller/FileController.java
+++ src/main/java/app/controller/FileController.java
@@ -33,8 +33,8 @@ public class FileController {
     @Autowired
     private FileService fileService;
     
-       @RequestMapping(value = "/references/{id}", method = RequestMethod.GET)
-    public ResponseEntity<byte[]> viewFile(@PathVariable Long id) {
+       @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> viewFile() {
         FileObject fo = fileService.createFile(referenceService.getReferences());
 
         final HttpHeaders headers = new HttpHeaders();
