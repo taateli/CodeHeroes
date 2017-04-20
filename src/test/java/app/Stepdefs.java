ec455766bcc2a115ba5fc486a94ff9ed77d37f2e
villepaa <ville.paavola@gmail.com>
2017-04-20 10:02:47 +0300

    test fix

--- src/test/java/app/Stepdefs.java
+++ src/test/java/app/Stepdefs.java
@@ -13,7 +13,6 @@ import java.io.File;
 
 import static org.junit.Assert.assertTrue;
 
-//import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -75,17 +74,18 @@ public class Stepdefs {
 
     @Then("^system will respond with \"([^\"]*)\"$")
     public void system_will_respond_with(String arg1) throws Throwable {
+//        Thread.sleep(2000);
         pageHasContent(arg1);
     }
 
 // This method checks if page has text that is given as parameter    
-    private void pageHasContent(String content) {
+    private void pageHasContent(String content) throws InterruptedException {
+                Thread.sleep(2000);
         assertTrue(driver.getPageSource().contains(content));
     }
 
     // this method fills the book form with mandatory fields and submits.
     private void createBookWithMandatoryFields(String key, String author, String title, String year, String publisher) throws InterruptedException {
-        Thread.sleep(2000);
         assertTrue(driver.getPageSource().contains("Add book reference"));
         WebElement element = driver.findElement(By.name("key"));
         element.sendKeys(key);
@@ -93,7 +93,6 @@ public class Stepdefs {
         element.sendKeys(author);
         element = driver.findElement(By.name("title"));
         element.sendKeys(title);
-        Thread.sleep(2000);
         element = driver.findElement(By.name("year"));
         element.sendKeys(year);
 
@@ -105,33 +104,7 @@ public class Stepdefs {
 
     }
 
-// this is method to create book reference with all possible fields    
-    private void createBookWithInputFields(String key, String author, String title, String year, String publisher, String month, String volume, String series, String edition) {
-        assertTrue(driver.getPageSource().contains("Add book reference"));
-        WebElement element = driver.findElement(By.name("key"));
-        element.sendKeys(key);
-        element = driver.findElement(By.name("author"));
-        element.sendKeys(author);
-        element = driver.findElement(By.name("title"));
-        element.sendKeys(title);
-        element = driver.findElement(By.name("address"));
-        element.sendKeys(title);
-        element = driver.findElement(By.name("year"));
-        element.sendKeys(year);
-        element = driver.findElement(By.name("publisher"));
-        element.sendKeys(publisher);
-        element = driver.findElement(By.name("month"));
-        element.sendKeys(month);
-        element = driver.findElement(By.name("vol"));
-        element.sendKeys(volume);
-        element = driver.findElement(By.name("series"));
-        element.sendKeys(series);
-        element = driver.findElement(By.name("edition"));
-        element.sendKeys(edition);
-        element = driver.findElement(By.name("save"));
-        element.submit();
 
-    }
     // all fields included - if missing, different message given
 
     public void createArticle(String key, String author, String title, String year,
