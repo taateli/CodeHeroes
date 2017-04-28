e8eabc72bd049deebecfdb66b08faf139fa7fe85
mustikka2015 <iisa.katka@helsinki.fi>
2017-04-28 09:08:20 +0300

    wingecko

---
+++ src/test/java/app/service/FileServiceTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.service;
+
+import app.domain.Book;
+import app.domain.FileObject;
+import app.domain.Reference;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author Iisa
+ */
+public class FileServiceTest {
+
+    FileService service;
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        service = new FileService();
+    }
+    
+    //Will be continued...
+}
