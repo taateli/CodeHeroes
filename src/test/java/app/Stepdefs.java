e9099e1eb642e68e61422f34f6f20e809baef276
Satu Bennert <bensatu@dhcp-asv-122.helsinki.fi>
2017-04-29 16:44:28 +0300

    refactoring stefDefs.java

--- src/test/java/app/Stepdefs.java
+++ src/test/java/app/Stepdefs.java
@@ -11,7 +11,6 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import java.io.File;
 import java.util.List;
-import static org.junit.Assert.assertFalse;
 
 import static org.junit.Assert.assertTrue;
 import org.openqa.selenium.Alert;
@@ -48,24 +47,27 @@ public class Stepdefs {
     @Given("^form book is selected$")
     public void form_book_is_selected() throws Throwable {
         driver.get(baseUrl + "/books");
+        pageHasContent("Add book reference");
     }
 
     @Given("^form inproceedings is selected$")
     public void form_inproceedings_is_selected() throws Throwable {
         driver.get(baseUrl + "/inproceedings");
+        pageHasContent("Add inproceedings reference");
     }
 
     @Given("^form article is selected$")
     public void form_article_is_selected() throws Throwable {
         driver.get(baseUrl + "/article");
+        pageHasContent("Add article reference");
     }
-    
-    
+
     @Given("^Front page is opened$")
     public void front_page_is_opened() throws Throwable {
         driver.get(baseUrl);
-    }
+        pageHasContent("Welcome to Reference manager");
 
+    }
 
     @Given("^Edit is pressed for key \"([^\"]*)\"$")
     public void edit_is_pressed_for_key(String key) throws Throwable {
@@ -73,15 +75,13 @@ public class Stepdefs {
         List<WebElement> lista = driver.findElements(By.tagName("td"));
         int indeksi = 0;
         for (int i = 0; i < lista.size(); i++) {
-            WebElement element = lista.get(i);
-            // there are four columns: checkbox, reference, tags, edit 
+            WebElement element = lista.get(i);    // there are four columns: checkbox, reference, tags, edit 
             if (element.getText().contains(key)) {
                 indeksi = i + 2; // element + 2 -> get the edit -element 
                 break;
             }
         }
-        if (indeksi > 0) {
-            // let's press the founded EDIT button (submit doesn't work)
+        if (indeksi > 0) {            // let's press the founded EDIT button (submit doesn't work)
             lista.get(indeksi).click();
         }
         Thread.sleep(2000);
@@ -90,7 +90,8 @@ public class Stepdefs {
     @Given("^a book reference with key \"([^\"]*)\" author \"([^\"]*)\" title \"([^\"]*)\" year \"([^\"]*)\" publisher \"([^\"]*)\" is created$")
     public void a_book_reference_with_key_author_title_year_publisher_is_created(String key, String author, String title, String year, String publisher) throws Throwable {
         form_book_is_selected();
-        createBookWithMandatoryFields(key, author, title, year, publisher);
+        String address = "", series = "", edition = "", month = "", volume = "", tags = "";
+        updateBook(key, author, title, year, publisher, address, series, edition, month, volume, tags);
         system_will_respond_with("Reference added successfully!");
     }
 
@@ -98,7 +99,8 @@ public class Stepdefs {
     @Given("^a book reference with key \"([^\"]*)\" author \"([^\"]*)\" title \"([^\"]*)\" year \"([^\"]*)\" publisher \"([^\"]*)\" is created succesfully$")
     public void a_book_reference_with_key_author_title_year_publisher_is_created_succesfully(String key, String author, String title, String year, String publisher) throws Throwable {
         form_book_is_selected();
-        createBookWithMandatoryFields(key, author, title, year, publisher);
+        String address = "", series = "", edition = "", month = "", volume = "", tags = "";
+        updateBook(key, author, title, year, publisher, address, series, edition, month, volume, tags);
         system_will_respond_with("Reference added successfully!");
     }
 
@@ -121,47 +123,34 @@ public class Stepdefs {
         driver.findElement(By.name("fileName")).click();
     }
 
-    @Then("^file is created by name \"([^\"]*)\"$")
-    public void file_is_created_by_name(String fileName) throws Throwable {
-        assertTrue(driver.getPageSource().contains("fileName"));
-
-    }
-
-    @Then("^system will not respond with \"([^\"]*)\"$")
-    public void system_will_not_respond_with(String content) throws Throwable {
-        pageHasNotContent(content);
-    }
-
     @When("^Search button is pressed$")
     public void search_button_is_pressed() throws Throwable {
         WebElement element = driver.findElement(By.name("search"));
         element.submit();
     }
 
-    @Then("^book reference with data \"([^\"]*)\" is displayd in the list$")
-    public void book_reference_with_data_is_displayd_in_the_list(String searchdata) throws Throwable {
-        system_will_respond_with(searchdata);
-    }
-
     @When("^key \"([^\"]*)\" author \"([^\"]*)\" title \"([^\"]*)\" year \"([^\"]*)\" publisher \"([^\"]*)\" are inserted$")
     public void key_author_title_year_publisher_are_inserted(String key, String author, String title, String year, String publisher) throws Throwable {
-        createBookWithMandatoryFields(key, author, title, year, publisher);
+        String address = "", series = "", edition = "", month = "", volume = "", tags = "";
+        updateBook(key, author, title, year, publisher, address, series, edition, month, volume, tags);
     }
 
     @When("^key \"([^\"]*)\" author \"([^\"]*)\" title \"([^\"]*)\" year \"([^\"]*)\" journal \"([^\"]*)\" publisher \"([^\"]*)\" volume \"([^\"]*)\" number \"([^\"]*)\" startingPage \"([^\"]*)\" endingPage \"([^\"]*)\" month \"([^\"]*)\" address \"([^\"]*)\" are inserted$")
     public void key_author_title_year_journal_publisher_volume_number_startingPage_endingPage_month_address_are_inserted(String key, String authors, String title, String year, String journal, String publisher, String volume, String number, String startingPage, String endingPage, String month, String address) throws Throwable {
-        createArticle(key, authors, title, year, journal, publisher, volume, number, startingPage, endingPage, month, address);
+        String tags = "";
+        updateArticle(key, authors, title, year, journal, publisher, volume, number, startingPage, endingPage, month, address, tags);
 
     }
 
     @When("^key \"([^\"]*)\" author \"([^\"]*)\" title \"([^\"]*)\" year \"([^\"]*)\" pubisher \"([^\"]*)\" editor \"([^\"]*)\" booktitle \"([^\"]*)\" address \"([^\"]*)\" series \"([^\"]*)\" startingPage \"([^\"]*)\" endingPage \"([^\"]*)\" month \"([^\"]*)\" organization \"([^\"]*)\" are inserted$")
     public void key_author_title_year_pubisher_editor_booktitle_address_series_startingPage_endingPage_month_organization_are_inserted(
             String key, String author, String title, String year, String publisher, String editor, String booktitle, String address, String series, String startingPage, String endingPage, String month, String organization) throws Throwable {
-        createInproceedings(key, author, title, year, publisher, editor, booktitle, address, series, startingPage, endingPage, month, organization);
+       String tags = "";
+        updateInproceedings(key, author, title, year, publisher, editor, booktitle, address, series, startingPage, endingPage, month, organization, tags);
     }
 
     @When("^Delete is pressed$")
-    public void delete_is_pressed() throws InterruptedException {
+    public void delete_() throws InterruptedException {
         // this will make the popup visible
         driver.findElement(By.xpath("/html/body/div/div/div/form[2]/input[2]")).click();
         Thread.sleep(2000); // If you wanna see the popup for 2 seconds
@@ -169,42 +158,16 @@ public class Stepdefs {
 
     @When("^popup is accepted$")
     public void popup_is_accepted() throws Throwable {
-        long timeout = 5000;
-        long waitForAlert = System.currentTimeMillis() + timeout;
-        boolean boolFound = false;
-        do {
-            try {
-                Alert alert = this.driver.switchTo().alert();
-                if (alert != null) {
-                    alert.accept(); // OK is accepted from the popup
-                    // alert.dismiss(); // Cancel is accepted from the popup
-                    boolFound = true;
-                }
-            } catch (NoAlertPresentException ex) {
-            }
-        } while ((System.currentTimeMillis() < waitForAlert) && (!boolFound));
-
+        boolean yes = true;
+        popup(yes);
     }
 
     @When("^popup is not accepted$")
     public void popup_is_not_accepted() throws Throwable {
-        long timeout = 500;
-        long waitForAlert = System.currentTimeMillis() + timeout;
-        boolean boolFound = false;
-        do {
-            try {
-                Alert alert = this.driver.switchTo().alert();
-                if (alert != null) {
-                    //  alert.accept(); // OK is accepted from the popup
-                    alert.dismiss(); // Cancel is accepted from the popup
-                    boolFound = true;
-                }
-            } catch (NoAlertPresentException ex) {
-            }
-        } while ((System.currentTimeMillis() < waitForAlert) && (!boolFound));
-
+        boolean no = false;
+        popup(no);
     }
-    
+
     @When("^a book reference with key \"([^\"]*)\" author \"([^\"]*)\" title \"([^\"]*)\" year \"([^\"]*)\" publisher \"([^\"]*)\" is updated$")
     public void a_book_reference_with_key_author_title_year_publisher_is_updated(String key, String author, String title, String year, String publisher) throws Throwable {
         //   System.out.println("Driverin page source onko true? " +driver.getPageSource().contains("Edit a book reference"));
@@ -219,37 +182,90 @@ public class Stepdefs {
 
     }
 
+    @Then("^file is created by name \"([^\"]*)\"$")
+    public void file_is_created_by_name(String fileName) throws Throwable {
+        assertTrue(driver.getPageSource().contains("fileName"));
+
+    }
+
+    @Then("^system will not respond with \"([^\"]*)\"$")
+    public void system_will_not_respond_with(String content) throws Throwable {
+        pageHasNotContent(content);
+    }
+
     @Then("^system will respond with \"([^\"]*)\"$")
     public void system_will_respond_with(String arg1) throws Throwable {
         Thread.sleep(4000);
         pageHasContent(arg1);
     }
 
+    @Then("^book reference with data \"([^\"]*)\" is displayd in the list$")
+    public void book_reference_with_data_is_displayd_in_the_list(String searchdata) throws Throwable {
+        system_will_respond_with(searchdata);
+    }
+
+// this method is true, if page has not the given text as a parameter
     private void pageHasNotContent(String content) throws InterruptedException {
         Thread.sleep(4000);
         assertTrue(!driver.getPageSource().contains(content));
     }
 
-// This method checks if page has text that is given as parameter    
+// This method is true, if page has the given text as a parameter    
     private void pageHasContent(String content) throws InterruptedException {
         Thread.sleep(4000);
         assertTrue(driver.getPageSource().contains(content));
     }
 
-    // this method fills the book form with mandatory fields and submits.
-    private void createBookWithMandatoryFields(String key, String author, String title, String year, String publisher) throws InterruptedException {
-        assertTrue(driver.getPageSource().contains("Add book reference"));
-        WebElement element = driver.findElement(By.name("key"));
-        element.sendKeys(key);
-        element = driver.findElement(By.name("authors"));
-        element.sendKeys(author);
-        element = driver.findElement(By.name("title"));
-        element.sendKeys(title);
-        element = driver.findElement(By.name("year"));
-        element.sendKeys(year);
+    private void popup(Boolean accept) {
+        long timeout = 5000;
+        long waitForAlert = System.currentTimeMillis() + timeout;
+        boolean boolFound = false;
+        do {
+            try {
+                Alert alert = this.driver.switchTo().alert();
+                if (alert != null) {
+                    if (accept) {
+                        alert.accept();
+                    }// OK is accepted from the popup
+                    else {
+                        alert.dismiss();
+                    } // Cancel is accepted from the popup
+                    boolFound = true;
+                }
+            } catch (NoAlertPresentException ex) {
+            }
+        } while ((System.currentTimeMillis() < waitForAlert) && (!boolFound));
 
+    }
+    private void updateField(WebElement element, String data){
+        element.sendKeys("");
+        element.sendKeys(data);
+        
+    }
+    // this method fills the book form with mandatory fields and submits.
+    private void updateBook(String key, String author, String title, String year, String publisher, String address, String series, String edition, String month, String volume, String tags) throws InterruptedException {
+        WebElement element = driver.findElement(By.name("key"));
+        updateField(element, key);
+        element = driver.findElement(By.name("authors"));        
+        updateField(element, author);
+        element = driver.findElement(By.name("title"));
+        updateField(element, title);
+        element = driver.findElement(By.name("year"));
+        updateField(element, year);
         element = driver.findElement(By.name("publisher"));
-        element.sendKeys(publisher);
+        updateField(element, publisher);
+        element = driver.findElement(By.name("address"));
+        updateField(element, address);
+        element = driver.findElement(By.name("series"));
+        updateField(element, series);
+        element = driver.findElement(By.name("edition"));
+        updateField(element, edition);
+        element = driver.findElement(By.name("month"));
+        updateField(element, month);
+        element = driver.findElement(By.name("volume"));
+        updateField(element, volume);
+        element = driver.findElement(By.name("tags"));
+        updateField(element, tags);
 
         element = driver.findElement(By.name("save"));
         element.submit();
@@ -257,69 +273,70 @@ public class Stepdefs {
     }
 
     // all fields included - if missing, different message given
-    public void createArticle(String key, String author, String title, String year,
-            String journal, String publisher, String volume, String number, String startingPage, String endingPage, String month, String address) {
-        assertTrue(driver.getPageSource().contains("Add article reference"));
-        WebElement element = driver.findElement(By.name("key"));
-        element.sendKeys(key);
+    public void updateArticle(String key, String author, String title, String year,
+            String journal, String publisher, String volume, String number, String startingPage, String endingPage, String month, String address, String tags) {
+            WebElement element = driver.findElement(By.name("key"));
+        updateField(element, key);
         element = driver.findElement(By.name("authors"));
-        element.sendKeys(author);
+        updateField(element, author);
         element = driver.findElement(By.name("title"));
-        element.sendKeys(title);
+        updateField(element, title);
         element = driver.findElement(By.name("year"));
-        element.sendKeys(year);
+        updateField(element, year);
         element = driver.findElement(By.name("journal"));
-        element.sendKeys(journal);
+        updateField(element, journal);
         element = driver.findElement(By.name("publisher"));
-        element.sendKeys(publisher);
+        updateField(element, publisher);
         element = driver.findElement(By.name("volume"));
-        element.sendKeys(volume);
+       updateField(element, volume);
         element = driver.findElement(By.name("number"));
-        element.sendKeys(number);
+        updateField(element, number);
         element = driver.findElement(By.name("startingPage"));
-        element.sendKeys(startingPage);
+       updateField(element, startingPage);
         element = driver.findElement(By.name("endingPage"));
-        element.sendKeys(endingPage);
+        updateField(element, endingPage);
         element = driver.findElement(By.name("month"));
-        element.sendKeys(month);
+       updateField(element, month);
         element = driver.findElement(By.name("address"));
-        element.sendKeys(title);
+       updateField(element, address);
+        element = driver.findElement(By.name("tags"));
+       updateField(element, tags);
 
+        element = driver.findElement(By.name("save"));
         element.submit();
     }
 
-    public void createInproceedings(String key, String author, String title, String year, String publisher,
-            String editor, String booktitle, String address, String series,
-            String startingPage, String endingPage, String month, String organization
-    ) {
-        assertTrue(driver.getPageSource().contains("Add inproceedings reference"));
+    public void updateInproceedings(String key, String author, String title, String year, String publisher, String editor, String booktitle, String address, String series, String startingPage, String endingPage, String month, String organization, String tags) {
         WebElement element = driver.findElement(By.name("key"));
-        element.sendKeys(key);
+        updateField(element, key);
         element = driver.findElement(By.name("authors"));
-        element.sendKeys(author);
+        updateField(element, author);
         element = driver.findElement(By.name("title"));
-        element.sendKeys(title);
+        updateField(element, title);
         element = driver.findElement(By.name("year"));
-        element.sendKeys(year);
+        updateField(element, year);
         element = driver.findElement(By.name("publisher"));
-        element.sendKeys(publisher);
+        updateField(element, publisher);
         element = driver.findElement(By.name("editor"));
-        element.sendKeys(editor);
+        updateField(element, editor);
         element = driver.findElement(By.name("bookTitle"));
-        element.sendKeys(booktitle);
+        updateField(element, booktitle);
         element = driver.findElement(By.name("address"));
-        element.sendKeys(address);
+       updateField(element, address);
         element = driver.findElement(By.name("series"));
-        element.sendKeys(series);
+        updateField(element, series);
         element = driver.findElement(By.name("startingPage"));
-        element.sendKeys(startingPage);
+        updateField(element, startingPage);
         element = driver.findElement(By.name("endingPage"));
-        element.sendKeys(endingPage);
+        updateField(element, endingPage);
         element = driver.findElement(By.name("month"));
-        element.sendKeys(month);
+        updateField(element, month);
         element = driver.findElement(By.name("organization"));
-        element.sendKeys(organization);
+        updateField(element, organization);
+        element = driver.findElement(By.name("tags"));
+        updateField(element, tags);
 
+        element = driver.findElement(By.name("save"));
         element.submit();
     }
 
